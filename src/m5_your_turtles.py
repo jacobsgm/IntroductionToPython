1e741bc99c5fb0a99dcb0c70ba4312f44bd50164
jacobsgm <jacobsgm@rose-hulman.edu>
2017-11-29 00:32:33 -0500

    i need help with this

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -35,10 +35,33 @@ window = rg.TurtleWindow()
 
 turtle1 = rg.SimpleTurtle('turtle')
 turtle1.pen = rg.Pen('green', 5)
-turtle1.speed = 50
+turtle1.speed = 10
 
 size = 50
 
 for k in range(5):
 
+    turtle1.draw_circle(50)
 
+    turtle1.pen_up()
+    turtle1.forward(100)
+    turtle1.right(100)
+    turtle1.right(150)
+    turtle1.right(100)
+
+    turtle1.pen_down()
+    size = size-5
+
+
+turtle2 = rg.SimpleTurtle('turtle')
+turtle2.pen = rg.Pen('pink', 12)
+turtle2.speed = 5
+
+for k in range(15):
+
+    turtle2.forward(100)
+    turtle2.right(100)
+    turtle2.forward(100)
+
+
+window.close_on_mouse_click()
